3efbd4939267325b6a50379e5e1a8220985e5707
Tobias Oberstein <tobias.oberstein@tavendo.de>
2011-08-10 16:25:36 +0200

    fix case bug

--- lib/python/autobahn/case/case9_6_1.py
+++ lib/python/autobahn/case/case9_6_1.py
@@ -20,7 +20,7 @@ from case import Case
 
 class Case9_6_1(Case):
 
-   DESCRIPTION = """Send binary message message with payload of length 1 * 2**20 (1M). Sent out data in chops of 4 octets."""
+   DESCRIPTION = """Send binary message message with payload of length 1 * 2**20 (1M). Sent out data in chops of 1 octet."""
 
    EXPECTATION = """Receive echo'ed binary message (with payload as sent)."""
 
