1757d5f00771610a21cc2f164d48b761e35a4153
Tobias Oberstein <tobias.oberstein@tavendo.de>
2011-08-12 18:01:35 +0200

    refactor to proper use of Twisted producer/consumer framework

--- demo/streaming/streaming_client.py
+++ demo/streaming/streaming_client.py
@@ -16,106 +16,100 @@
 ##
 ###############################################################################
 
-import hashlib, random, struct, binascii, gc
-from twisted.internet import reactor
+import random, struct
+from zope.interface import implements
+from twisted.internet import reactor, interfaces
 from autobahn.websocket import WebSocketProtocol, WebSocketClientFactory, WebSocketClientProtocol
 
 
-class StreamingClientProtocol(WebSocketClientProtocol):
+FRAME_SIZE = 0x7FFFFFFFFFFFFFFF # 2^63 - This is the maximum imposed by the WS protocol
+#FRAME_SIZE = 64 * 2**10 # 64k
+#FRAME_SIZE = 1 * 2**20 # 1M
+
+
+class RandomBytesSender:
    """
-   Streaming WebSockets client that sends random bits, all sent in 1 WebSockets
-   message within frames of length 2^63.
-   It can run forever in constant memory (more precisely, with a finite upper
-   bound, and no bugs in GC).
+   A Twisted Push Producer generating a stream of random octets.
    """
+   implements(interfaces.IPushProducer)
 
-   ## We send data within the 1 message in frames of this size
-   ##
-   FRAME_SIZE = 0x7FFFFFFFFFFFFFFF # 2^63 - This is the maximum imposed by the WS protocol
-
-   ## We send data within frames in chunks of BUFFER_SIZE bytes
-   ##
-   SEND_SIZE = 64 * 2**10
-
-   ## We want updates of the running digest every BATCH_SIZE bytes
-   ##
-   BATCH_SIZE = 1 * 2**20
-
-
-   def onOpen(self):
-      ## batch counter for batches send by client
-      self.count = 0
-
-      ## batch counter for batch digests received by server
-      self.scount = 0
-
-      ## we'll compute the digest also ourselfes, since this
-      ## is a demo, and we want to verify everything works
-      self.sha256 = hashlib.sha256()
-
-      ## we start a binary message
-      self.beginMessage(opcode = WebSocketProtocol.MESSAGE_TYPE_BINARY)
-
-      ## we start a new frame within the message
-      ##
-      self.beginMessageFrame(StreamingClientProtocol.FRAME_SIZE)
-
-      ## then we send 4 batches out .. this is to get the server going
-      ##
-      for i in xrange(0, 3):
-         self.sendBatch()
-
-
-   def sendBatch(self):
-      ## compute random bits for batch and send data on the fly
-      ##
-      i = 0
-      while i < StreamingClientProtocol.BATCH_SIZE:
-         data = self.randomByteString(StreamingClientProtocol.SEND_SIZE)
-         self.sha256.update(data)
-
-         ## now send the data streaming within the frame ..
-         ##
-         if self.sendMessageFrameData(data) <= 0:
-            ## Note, this will only be reached when frame size was reached.
-            ## In that case, we just start another fragment in the message.
-            ## When max. frame size 2^63 is chosen, we practically will never
-            ## come here!
-            self.beginMessageFrame(StreamingClientProtocol.FRAME_SIZE)
-            print "new frame started!"
-         i += StreamingClientProtocol.SEND_SIZE
-
-      ## we compute the digest also ourselfes for checking
-      digest = self.sha256.hexdigest()
-      print "Digest up to batch %d computed by me: %s.." % (self.count, digest[:8])
-      self.count += 1
-
-
-   def onMessage(self, msg, binary):
-      print "Digest up to batch %d computed by server: %s.." % (self.scount, msg[:8])
-      self.scount += 1
-
-      ## upon receiving computed digest from server, we queue up
-      ## another chunk .. essentially, this is our flow control - otherwise
-      ## we would enqueue chunks in the Twisted/TCP/IP stack as fast as we could
-      ## and consume more and more memory.
-      self.sendBatch()
 
    def randomByteString(self, len):
+      """
+      Generate a string of random bytes.
+      """
       return ''.join([struct.pack("!Q", random.getrandbits(64)) for x in xrange(0, len / 8 + int(len % 8 > 0))])[:len]
 
+   def __init__(self, proto):
+      self.proto = proto
+      self.started = False
+      self.paused = False
 
-class StreamingClientFactory(WebSocketClientFactory):
+   def pauseProducing(self):
+      """
+      Called when we are asked to pause producing/sending, because the
+      receiver is busy consuming our stuff.
+      """
+      self.paused = True
 
-   protocol = StreamingClientProtocol
+   def resumeProducing(self):
+      """
+      Called when we are asked to resume producing/sending, because the
+      receiver has consumed and can take more.
+      """
+      self.paused = False
 
-   def __init__(self, debug = False):
-      self.path = "/"
-      self.debug = debug
+      if not self.started:
+         ## upon starting, we create a new WebSockets binary message and
+         ## begin a new frame ..
+         self.proto.beginMessage(opcode = WebSocketProtocol.MESSAGE_TYPE_BINARY)
+         self.proto.beginMessageFrame(FRAME_SIZE)
+         self.started = True
+
+      while not self.paused:
+         ## while running, we generate random bits ..
+         data = self.randomByteString(1024)
+
+         ## .. send them as frame payload
+         if self.proto.sendMessageFrameData(data) <= 0:
+
+            ## when the frame is finished, we get here, and
+            ## we just start a new frame.
+            self.proto.beginMessageFrame(FRAME_SIZE)
+            print "new frame started!"
+
+   def stopProducing(self):
+        pass
+
+
+class HashClientProtocol(WebSocketClientProtocol):
+   """
+   Streaming WebSockets client that generates stream of random octets
+   sent to streaming WebSockets server, which computes a running SHA-256,
+   which it will send every BATCH_SIZE octets back to us.
+
+   This client can run forever in constant memory (more precisely, with a
+   finite upper bound, and with no bugs in GC).
+   """
+
+   def onOpen(self):
+      self.scount = 0
+
+      ## when WebSockets handshake is complete, we create our producer,
+      ## register and resume it
+      ##
+      producer = RandomBytesSender(self)
+      self.registerProducer(producer, True)
+      producer.resumeProducing()
+
+   def onMessage(self, msg, binary):
+      print "Digest for batch %d computed by server: %s.." % (self.scount, msg[:8])
+      self.scount += 1
 
 
 if __name__ == '__main__':
 
-   factory = StreamingClientFactory()
+   factory = WebSocketClientFactory()
+   factory.protocol = HashClientProtocol
    reactor.connectTCP("localhost", 9000, factory)
    reactor.run()
--- demo/streaming/streaming_server.py
+++ demo/streaming/streaming_server.py
@@ -18,24 +18,29 @@
 
 import sys, hashlib
 from twisted.internet import reactor
-from twisted.python import log
 from autobahn.websocket import WebSocketServerFactory, WebSocketServerProtocol
-from streaming_client import StreamingClientProtocol
 
-class StreamingServerProtocol(WebSocketServerProtocol):
+
+## We want updates of the running digest every BATCH_SIZE bytes
+##
+BATCH_SIZE = 1 * 2**20
+
+
+class HashServerProtocol(WebSocketServerProtocol):
    """
    Streaming WebSockets server that computes a running SHA-256 for data
    received. It will respond every BATCH_SIZE bytes with the digest
    up to that point.
-   It can run forever in constant memory (more precisely, with a finite upper
-   bound, and with no bugs in GC).
+
+   This server can run forever in constant memory (more precisely, with a
+   finite upper bound, and with no bugs in GC).
    """
 
    def sendDigest(self, final = False):
       digest = self.sha256.hexdigest()
       self.sendMessage(digest)
       if not final:
-         print "Sent digest up to batch %d : %s.." % (self.count, digest[:8])
+         print "Sent digest for batch %d : %s.." % (self.count, digest[:8])
       else:
          print "Sent final digest for full message : %s" % digest
 
@@ -44,11 +49,11 @@ class StreamingServerProtocol(WebSocketServerProtocol):
       self.sha256 = hashlib.sha256()
       self.count = 0
       self.received = 0
-      self.next = StreamingClientProtocol.BATCH_SIZE
+      self.next = BATCH_SIZE
 
 
    def onMessageFrameBegin(self, length, reserved):
-      pass
+      print "new frame of length %d started" % length
 
 
    def onMessageFrameData(self, data):
@@ -62,7 +67,7 @@ class StreamingServerProtocol(WebSocketServerProtocol):
          rest = length - (self.received - self.next)
          self.sha256.update(str(data[:rest]))
          self.sendDigest()
-         self.next += StreamingClientProtocol.BATCH_SIZE
+         self.next += BATCH_SIZE
          self.count += 1
 
          ## .. and update the digest for the rest
@@ -82,17 +87,9 @@ class StreamingServerProtocol(WebSocketServerProtocol):
       self.sendDigest(True)
 
 
-class StreamingServerFactory(WebSocketServerFactory):
-
-   protocol = StreamingServerProtocol
-
-   def __init__(self, debug = False):
-      self.debug = debug
-
-
 if __name__ == '__main__':
 
-   log.startLogging(sys.stdout)
-   factory = StreamingServerFactory()
+   factory = WebSocketServerFactory()
+   factory.protocol = HashServerProtocol
    reactor.listenTCP(9000, factory)
    reactor.run()
