e4a239187f90cb923ac7aa05e852b8612c6aa640
Peter Thorson <github@zaphoyd.com>
2011-09-22 14:28:36 -0500

    started working on case 7.3

--- lib/python/autobahn/case/case7_3.py
+++ lib/python/autobahn/case/case7_3.py
@@ -18,16 +18,52 @@
 
 from case import Case
 
-class Case7_2(Case):
+class Case7_3_1(Case):
 
-   DESCRIPTION = """Send ping after close"""
+   DESCRIPTION = """Send invalid close frame"""
 
    EXPECTATION = """Receive echo'ed text message. Clean close with normal code."""
-
+   
+   def init(self):
+      self.plen = 0;
+      self.payload = ""
+   
    def onOpen(self):
       payload = "Hello World!"
+      
+      
+      plen = 0
+      payload = ""
+
+      if code is not None:
+
+         if (not (code >= 3000 and code <= 3999)) and \
+            (not (code >= 4000 and code <= 4999)) and \
+            code not in WebSocketProtocol.CLOSE_STATUS_CODES_ALLOWED:
+            raise Exception("invalid status code %d for close frame" % code)
+
+         payload = struct.pack("!H", code)
+         plen = 2
+
+         if reason is not None:
+            reason = reason.encode("UTF-8")
+            plen += len(reason)
+         else:
+            reason = ""
+
+         if plen > 125:
+            raise Exception("close frame payload larger than 125 octets")
+
+         payload += reason
+
+      else:
+         if reason is not None and reason != "":
+            raise Exception("status reason '%s' without status code in close frame" % reason)
+      self.sendFrame(opcode = 8, payload = payload)
+      
+      
       self.expected[Case.OK] = [("message", payload, False)]      
-      self.expectedClose = {"failedByMe":True,"closeCode":self.p.CLOSE_STATUS_CODE_NORMAL,"requireClean":True}
+      self.expectedClose = {"failedByMe":False,"closeCode":self.p.CLOSE_STATUS_CODE_NORMAL,"requireClean":True}
       self.p.sendFrame(opcode = 1, payload = payload)
       self.p.sendClose(self.p.CLOSE_STATUS_CODE_NORMAL);
       self.p.sendFrame(opcode = 9)
