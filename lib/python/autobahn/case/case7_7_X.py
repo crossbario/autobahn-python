d67dc52171c48230294dbc2e4a8ebd542e054f85
Peter Thorson <github@zaphoyd.com>
2011-10-25 08:34:53 -0500

    adds cases 7.7.*

--- lib/python/autobahn/case/case7_7_X.py
+++ lib/python/autobahn/case/case7_7_X.py
@@ -22,68 +22,27 @@ from case import Case
 tests = [1000,1001,1002,1003,1007,1008,1009,1010,1011,3000,4999]
 
 Case7_7_X = []
-Case7_8_X = []
-
 
 def __init__(self, protocol):
    Case.__init__(self, protocol)
-   self.reportTime = True
 
 def onOpen(self):
-   self.behavior = Case.FAILED
-   self.expectedClose = {"failedByMe": True, "closeCode": self.p.CLOSE_STATUS_CODE_NORMAL, "requireClean": True}
-   self.result = "Case did not finish within %d seconds." % self.WAITSECS
-   self.p.closeAfter(self.WAITSECS)
-   self.count = 0
-   self.sendOne()
+   self.expected[Case.OK] = []      
+   self.expectedClose = {"failedByMe":True,"closeCode":[self.p.CLOSE_STATUS_CODE_NORMAL,self.CLOSE_CODE],"requireClean":True}
+   self.p.sendCloseFrame(self.CLOSE_CODE, reasonUtf8 = "")
+   self.p.killAfter(1)
 
-def sendOne(self):
-   if self.BINARY:
-      self.p.sendFrame(opcode = 2, payload = "\xfe", payload_len = self.LEN)
-   else:
-      self.p.sendFrame(opcode = 1, payload = "*", payload_len = self.LEN)
-   self.count += 1
-
-def onMessage(self, msg, binary):
-   if binary != self.BINARY or len(msg) != self.LEN:
-      self.behavior = Case.FAILED
-      self.result = "Echo'ed message type or length differs from what I sent (got binary = %s, payload length = %s)." % (binary, len(msg))
-      self.p.enableWirelog(True)
-      self.p.sendClose(self.p.CLOSE_STATUS_CODE_NORMAL)
-   elif self.count < self.COUNT:
-      self.sendOne()
-   else:
-      self.behavior = Case.OK
-      self.result = "Ok, received all echo'ed messages in time."
-      self.p.enableWirelog(True)
-      self.p.sendClose(self.p.CLOSE_STATUS_CODE_NORMAL)
-
-for b in [False, True]:
-   i = 1
-   for s in tests:
-      if b:
-         mt = "binary"
-         cc = "Case9_8_%d"
-      else:
-         mt = "text"
-         cc = "Case9_7_%d"
-      DESCRIPTION = """Send %d %s messages of payload size %d to measure implementation/network RTT (round trip time) / latency.""" % (s[1], mt, s[0])
-      EXPECTATION = """Receive echo'ed %s messages (with payload as sent). Timeout case after %d secs.""" % (mt, s[2])
-      C = type(cc % i,
-                (object, Case, ),
-                {"LEN": s[0],
-                 "COUNT": s[1],
-                 "WAITSECS": s[2],
-                 "BINARY": b,
-                 "DESCRIPTION": """%s""" % DESCRIPTION,
-                 "EXPECTATION": """%s""" % EXPECTATION,
-                 "__init__": __init__,
-                 "onOpen": onOpen,
-                 "onMessage": onMessage,
-                 "sendOne": sendOne,
-                 })
-      if b:
-         Case9_8_X.append(C)
-      else:
-         Case9_7_X.append(C)
-      i += 1
+i = 1
+for s in tests:
+   DESCRIPTION = """Send close with close code %d""" % s
+   EXPECTATION = """Clean close with normal or echoed code"""
+   C = type("Case7_7_%d" % i,
+			(object, Case, ),
+			{"CLOSE_CODE": s,
+			 "DESCRIPTION": """%s""" % DESCRIPTION,
+			 "EXPECTATION": """%s""" % EXPECTATION,
+			 "__init__": __init__,
+			 "onOpen": onOpen,
+			 })
+   Case7_7_X.append(C)
+   i += 1
