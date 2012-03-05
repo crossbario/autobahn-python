06bf3ed10dfc193d47163697b160fea7d8dd70c1
Tobias Oberstein <tobias.oberstein@tavendo.de>
2012-03-05 17:46:04 +0100

    started sequencing of tests

--- testsuite/wsperf/wsperf_commander.py
+++ testsuite/wsperf/wsperf_commander.py
@@ -1,6 +1,6 @@
 ###############################################################################
 ##
-##  Copyright 2011 Tavendo GmbH
+##  Copyright 2012 Tavendo GmbH
 ##
 ##  Licensed under the Apache License, Version 2.0 (the "License");
 ##  you may not use this file except in compliance with the License.
@@ -20,21 +20,43 @@ import sys, json, pprint
 from twisted.internet import reactor
 from twisted.python import log
 from autobahn.websocket import WebSocketClientFactory, WebSocketClientProtocol, connectWS
+from autobahn.util import newid
 
-TESTCMD = """message_test:uri=ws://192.168.1.120:9000/;token=foo;size=4096;count=1000;timeout=10000;binary=true;sync=false;correctness=exact;"""
+TESTCMD = """message_test:uri=ws://192.168.1.120:9000/;token=%(token)s;size=%(size)d;count=10000;timeout=10000;binary=true;sync=true;correctness=exact;"""
 
+SERVERS = [{'name': 'Autobahn/0.4.11', 'uri': 'ws://192.168.1.120:9000'},
+           {'name': 'WebSocket++/HEAD', 'uri': 'ws://192.168.1.141:9002'}]
+
+SIZES = [0, 16, 64, 256, 1024, 4096]
+#SIZES = [0]
 
 class WsPerfCommanderProtocol(WebSocketClientProtocol):
 
+   def sendNext(self):
+      id = newid()
+      cmd = TESTCMD % {'size': SIZES[self.current],
+                       'token': id}
+      print cmd
+      self.sendMessage(cmd)
+      self.current += 1
+
    def onOpen(self):
       self.pp = pprint.PrettyPrinter(indent = 3)
-      self.sendMessage(TESTCMD)
+      self.current = 0
+      self.sendNext()
 
    def onMessage(self, msg, binary):
       if not binary:
          try:
             o = json.loads(msg)
-            self.pp.pprint(o)
+            if o['type'] == u'test_complete':
+               if self.current < len(SIZES) - 1:
+                  self.sendNext()
+               else:
+                  print "ALL TESTS COMPLETE!"
+                  reactor.stop()
+            elif o['type'] == u'test_data':
+               self.pp.pprint(o)
          except ValueError, e:
             pass
 
