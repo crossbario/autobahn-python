d1ec8fd8be5d41930fde89c686fb86425dea4e88
Tobias Oberstein <tobias.oberstein@tavendo.de>
2013-05-16 10:53:25 +0200

    fix: When passing a unicode object as authSecret hmac.new fails, because it expects a byte type.

--- autobahn/autobahn/wamp.py
+++ autobahn/autobahn/wamp.py
@@ -1540,6 +1540,8 @@ class WampCraProtocol(WampProtocol):
       """
       if authSecret is None:
          authSecret = ""
+      if isinstance(authSecret, unicode):
+         authSecret = authSecret.encode('utf8')
       authSecret = WampCraProtocol.deriveKey(authSecret, authExtra)
       h = hmac.new(authSecret, authChallenge, hashlib.sha256)
       sig = binascii.b2a_base64(h.digest()).strip()
