a7b70e038c9ac928fb70bda457140150c7eec05b
Richard Klemm <richy@coding-reality.de>
2013-09-01 12:02:14 +0200

    tavendo/AutobahnPython#148: Replaced pbkdf2 module; Makde pbkdf2 module an extra requirement

--- autobahn/setup.py
+++ autobahn/setup.py
@@ -65,7 +65,8 @@ setup (
    install_requires = ['setuptools', 'Twisted>=11.1'],
    extras_require = {
       'accelerate': ["wsaccel>=0.6.2"],
-      'compress': ["python-snappy>=0.5", "lz4>=0.2.1"]
+      'compress': ["python-snappy>=0.5", "lz4>=0.2.1"],
+      'pbkdf2': ["pbkdf2>=1.3"]
    },
    packages = ['autobahn'],
    zip_safe = False,
