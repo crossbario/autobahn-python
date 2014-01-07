bb2ce418510fd15c41e211659671e4b986b554a1
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-01-07 14:22:08 +0100

    add new example

---
+++ examples/asyncio/websocket/slowsquare/client.py
@@ -0,0 +1,55 @@
+###############################################################################
+##
+##  Copyright (C) 2014 Tavendo GmbH
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##
+###############################################################################
+
+from autobahn.asyncio.websocket import WebSocketClientProtocol, \
+                                       WebSocketClientFactory
+
+import asyncio
+import json, random
+
+
+class SlowSquareClientProtocol(WebSocketClientProtocol):
+
+   def onOpen(self):
+      x = 10. * random.random()
+      self.sendMessage(json.dumps(x).encode('utf8'))
+      print("Request to square {} sent.".format(x))
+
+   def onMessage(self, payload, isBinary):
+      if not isBinary:
+         res = json.loads(payload.decode('utf8'))
+         print("Result received: {}".format(res))
+         self.sendClose()
+
+   def onClose(self, wasClean, code, reason):
+      loop.close()
+
+
+
+if __name__ == '__main__':
+
+   import asyncio
+
+   factory = WebSocketClientFactory("ws://localhost:9000", debug = False)
+   factory.protocol = SlowSquareClientProtocol
+
+   loop = asyncio.get_event_loop()
+   coro = loop.create_connection(factory, '127.0.0.1', 9000)
+   loop.run_until_complete(coro)
+   loop.run_forever()
+#   loop.close()
---
+++ examples/asyncio/websocket/slowsquare/server.py
@@ -0,0 +1,59 @@
+###############################################################################
+##
+##  Copyright (C) 2014 Tavendo GmbH
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##
+###############################################################################
+
+from autobahn.asyncio.websocket import WebSocketServerProtocol, \
+                                       WebSocketServerFactory
+
+import asyncio
+import json
+
+
+class SlowSquareServerProtocol(WebSocketServerProtocol):
+
+   @asyncio.coroutine
+   def slowsquare(self, x):
+      yield from asyncio.sleep(1)
+      return x * x
+
+   @asyncio.coroutine
+   def onMessage(self, payload, isBinary):
+      if not isBinary:
+         x = json.loads(payload.decode('utf8'))
+         res = yield from self.slowsquare(x)
+         self.sendMessage(json.dumps(res).encode('utf8'))
+
+
+
+if __name__ == '__main__':
+
+   import asyncio
+
+   factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
+   factory.protocol = SlowSquareServerProtocol
+
+   loop = asyncio.get_event_loop()
+   coro = loop.create_server(factory, '127.0.0.1', 9000)
+   server = loop.run_until_complete(coro)
+
+   try:
+      loop.run_forever()
+   except KeyboardInterrupt:
+      pass
+   finally:
+      server.close()
+      loop.close()
