ab909d84b356091402334cb71d3d97a996bbc58c
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-02-13 16:51:28 +0100

    more examples

---
+++ examples/twisted/wamp/basic/rpc/errors/backend.js
@@ -0,0 +1,31 @@
+try {
+   var autobahn = require('autobahn');
+} catch (e) {
+   // when running in browser, AutobahnJS will
+   // be included without a module system
+}
+
+var connection = new autobahn.Connection({
+   url: 'ws://127.0.0.1:9000/',
+   realm: 'realm1'}
+);
+
+connection.onopen = function (session) {
+
+   function sqrt(args) {
+      var x = args[0];
+      if (x === 0) {
+         throw "don't ask folly questions;)";
+      }
+      var res = Math.sqrt(x);
+      if (res !== res) {
+         //throw "cannot take sqrt of negative";
+         throw new autobahn.Error('com.myapp.error', ['fuck'], {a: 23, b: 9});
+      }
+      return res;
+   }
+
+   session.register(sqrt, 'com.myapp.sqrt');
+};
+
+connection.open();
