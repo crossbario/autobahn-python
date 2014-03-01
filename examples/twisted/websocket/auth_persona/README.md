bf3c66d3adba80b59065dbe753a2383188ecdb06
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-03-01 16:08:36 +0100

    add Mozilla Persona example

---
+++ examples/twisted/websocket/auth_persona/README.md
@@ -0,0 +1,23 @@
+WebSocket Authentication with Mozilla Persona
+=============================================
+
+This is a variant of a basic WebSocket Echo server that is running as a *Twisted Web Resource*.
+
+Running
+-------
+
+Run the server by doing
+
+    python server.py
+
+and open
+
+    http://localhost:8080/
+
+in your browser.
+
+To activate debug output on the server, start it
+
+    python server.py debug
+
+This will show up all WebSocket messages exchanged between clients and server.
---
+++ examples/twisted/websocket/auth_persona/server.py
@@ -0,0 +1,236 @@
+###############################################################################
+##
+##  Copyright (C) 2011-2014 Tavendo GmbH
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##
+###############################################################################
+
+import sys
+import json
+import urllib
+import Cookie
+
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.web.server import Site
+from twisted.web.static import File
+
+from autobahn.util import newid, utcnow
+from autobahn.websocket import http
+
+from autobahn.twisted.websocket import WebSocketServerFactory, \
+                                       WebSocketServerProtocol
+
+from autobahn.twisted.resource import WebSocketResource
+
+
+
+class PersonaServerProtocol(WebSocketServerProtocol):
+   """
+   WebSocket server protocol that tracks WebSocket connections using HTTP cookies,
+   and authenticates WebSocket connections using Mozilla Persona.
+   """
+
+   def onConnect(self, request):
+
+      ## This is called during the initial WebSocket opening handshake.
+
+      protocol, headers = None, {}
+
+      ## our cookie tracking ID
+      self._cbtid = None
+
+      ## see if there already is a cookie set ..
+      if request.headers.has_key('cookie'):
+         try:
+            cookie = Cookie.SimpleCookie()
+            cookie.load(str(request.headers['cookie']))
+         except Cookie.CookieError:
+            pass
+         else:
+            if cookie.has_key('cbtid'):
+               cbtid = cookie['cbtid'].value
+               if self.factory._cookies.has_key(cbtid):
+                  self._cbtid = cbtid
+                  log.msg("Cookie already set: %s" % self._cbtid)
+
+      ## if no cookie is set, create a new one ..
+      if self._cbtid is None:
+
+         self._cbtid = newid()
+         maxAge = 86400
+
+         cbtData = {'created': utcnow(),
+                    'authenticated': None,
+                    'maxAge': maxAge,
+                    'connections': set()}
+
+         self.factory._cookies[self._cbtid] = cbtData
+
+         ## do NOT add the "secure" cookie attribute! "secure" refers to the
+         ## scheme of the Web page that triggered the WS, not WS itself!!
+         ##
+         headers['Set-Cookie'] = 'cbtid=%s;max-age=%d' % (self._cbtid, maxAge)
+         log.msg("Setting new cookie: %s" % self._cbtid)
+
+      ## add this WebSocket connection to the set of connections
+      ## associated with the same cookie
+      self.factory._cookies[self._cbtid]['connections'].add(self)
+
+      ## accept the WebSocket connection, speaking subprotocol `protocol`
+      ## and setting HTTP headers `headers`
+      return (protocol, headers)
+
+
+   def onOpen(self):
+
+      ## This is called when initial WebSocket opening handshake has
+      ## been completed.
+
+      ## see if we are authenticated ..
+      authenticated = self.factory._cookies[self._cbtid]['authenticated']
+
+      if not authenticated:
+         ## .. if not, send authentication request
+         self.sendMessage(json.dumps({'cmd': 'AUTHENTICATION_REQUIRED'}))
+      else:
+         ## .. if yes, send info on authenticated user
+         self.sendMessage(json.dumps({'cmd': 'AUTHENTICATED', 'email': authenticated}))
+
+
+   def onClose(self, wasClean, code, reason):
+
+      ## This is called when WebSocket connection is gone
+
+      ## remove this connection from list of connections associated with
+      ## same cookie
+      self.factory._cookies[self._cbtid]['connections'].remove(self)
+
+      ## if list gets empty, possibly do something ..
+      if not self.factory._cookies[self._cbtid]['connections']:
+         log.msg("All connections for {} gone".format(self._cbtid))
+
+
+   def onMessage(self, payload, isBinary):
+
+      ## This is called when we receive a WebSocket message
+
+      if not isBinary:
+
+         msg = json.loads(payload)
+
+         if msg['cmd'] == 'AUTHENTICATE':
+
+            ## The client did it's Mozilla Persona authentication thing
+            ## and now wants to verify the authentication and login.
+            assertion = msg.get('assertion')
+
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            body = urllib.urlencode({'audience': 'http://192.168.1.130:8080/', 'assertion': assertion})
+
+            # {
+            #    "audience": "http://192.168.1.130:8080/",
+            #    "expires": 1393681951257,
+            #    "issuer": "gmail.login.persona.org",
+            #    "email": "tobias.oberstein@gmail.com",
+            #    "status": "okay"
+            # }
+
+            from twisted.web.client import getPage
+            d = getPage(url = "https://verifier.login.persona.org/verify",
+                        method = 'POST',
+                        postdata = body,
+                        headers = headers)
+
+            log.msg("Authentication request sent.")
+
+            def done(res):
+               res = json.loads(res)
+               if res['status'] == 'okay':
+                  ## Mozilla Persona successfully authenticated the user
+
+                  ## remember the user's email address. this marks the cookie as
+                  ## authenticated
+                  self.factory._cookies[self._cbtid]['authenticated'] = res['email']
+
+                  ## inform _all_ WebSocket connections of the successful auth.
+                  msg = json.dumps({'cmd': 'AUTHENTICATED', 'email': res['email']})
+                  for proto in self.factory._cookies[self._cbtid]['connections']:
+                     proto.sendMessage(msg)
+
+                  log.msg("Authenticated user {}".format(res['email']))
+               else:
+                  log.msg("Authentication failed!")
+                  self.sendMessage(json.dumps({'cmd': 'AUTHENTICATION_FAILED'}))
+                  self.sendClose()
+
+            def error(err):
+               log.msg("Authentication request failed: {}".format(err.value))
+               self.sendMessage(json.dumps({'cmd': 'AUTHENTICATION_FAILED'}))
+               self.sendClose()
+
+            d.addCallbacks(done, error)
+
+         elif msg['cmd'] == 'LOGOUT':
+
+            ## user wants to logout ..
+            if self.factory._cookies[self._cbtid]['authenticated']:
+               self.factory._cookies[self._cbtid]['authenticated'] = False
+
+               ## inform _all_ WebSocket connections of the logout
+               msg = json.dumps({'cmd': 'LOGGED_OUT'})
+               for proto in self.factory._cookies[self._cbtid]['connections']:
+                  proto.sendMessage(msg)
+
+               #self.sendClose()
+
+         else:
+            log.msg("unknown command {}".format(msg))
+
+
+
+class PersonaServerFactory(WebSocketServerFactory):
+   """
+   WebSocket server factory with cookie/sessions map.
+   """
+
+   protocol = PersonaServerProtocol
+
+   def __init__(self, url):
+      WebSocketServerFactory.__init__(self, url, debug = False)
+
+      ## map of cookies
+      self._cookies = {}
+
+
+
+if __name__ == '__main__':
+
+   log.startLogging(sys.stdout)
+
+   ## our WebSocket server factory
+   factory = PersonaServerFactory("ws://localhost:8080")
+
+   ## we serve static files under "/" ..
+   root = File(".")
+
+   ## .. and our WebSocket server under "/ws"
+   resource = WebSocketResource(factory)
+   root.putChild("ws", resource)
+
+   ## run both under one Twisted Web Site
+   site = Site(root)
+   reactor.listenTCP(8080, site)
+
+   reactor.run()
