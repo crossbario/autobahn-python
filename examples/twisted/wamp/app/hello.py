2d161e5f5625b3c5efe51045310ab321c36413b7
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-05-26 14:31:17 +0200

    make it look exactly like flask hello world

--- examples/twisted/wamp/app/hello.py
+++ examples/twisted/wamp/app/hello.py
@@ -1,12 +1,9 @@
 from autobahn.twisted.wamp import Application
-
 app = Application("ws://localhost:8080/ws", "realm1")
-
 
 @app.procedure('com.example.hello')
 def hello():
    return "Hello World!"
-
 
 if __name__ == "__main__":
    app.run()
