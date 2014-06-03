88155fc72da79db12ddf18fd60cedc920b3ec9c1
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-06-03 02:25:12 +0200

    adjust example

--- examples/twisted/wamp/app/hello.py
+++ examples/twisted/wamp/app/hello.py
@@ -1,9 +1,29 @@
-from autobahn.twisted.wamp import Application
-app = Application("ws://localhost:8080/ws", "realm1")
+import sys
+sys.path.insert(0, "../../../../autobahn")
 
-@app.procedure('com.example.hello')
+from twisted.internet.defer import returnValue
+
+from autobahn.twisted.app import Application
+app = Application('com.example')
+
+
+@app.register()
+def add2(a, b):
+   print("add2() called")
+   return a + b
+
+
+@app.register('com.example.hello')
 def hello():
-   return "Hello World!"
+   print("hello() called")
+   res = yield app.session.call('com.example.add2', 2, 3)
+   returnValue("Hello: {}".format(res))
+
+
+@app.signal('onpostjoin')
+def onpostjoin():
+   print("realm joined!")
+
 
 if __name__ == "__main__":
-   app.run()
+   app.run("ws://localhost:8080/ws", "realm1", standalone = True)
