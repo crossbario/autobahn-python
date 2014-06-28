897a5a970efc5ee1c9dcfbec89f2e6ee3ab665b7
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-06-28 13:29:08 +0200

    new example

---
+++ examples/twisted/wamp/app/klein/server.py
@@ -0,0 +1,81 @@
+###############################################################################
+##
+##  Copyright (C) 2014 Tavendo GmbH
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##
+###############################################################################
+
+
+import jinja2
+from klein import Klein
+
+from twisted.internet.defer import inlineCallbacks, returnValue
+from autobahn.twisted.wamp import Application
+
+
+
+## This is our WAMP application
+##
+wampapp = Application('com.example')
+
+
+@wampapp.register()
+def square(x):
+   print("square() called with {}".format(x))
+   return x * x
+
+
+
+## This is our Web application
+##
+webapp = Klein()
+webapp.visits = 0
+webapp.templates = jinja2.Environment(loader = jinja2.FileSystemLoader('.'))
+
+
+@webapp.route('/')
+def home(request):
+   webapp.visits += 1
+   wampapp.session.publish('com.example.onvisit', visits = webapp.visits)
+   page = webapp.templates.get_template('index.html')
+   return page.render(visits = webapp.visits)
+
+
+@webapp.route('/square/<int:x>')
+@inlineCallbacks
+def square(request, x):
+   result = yield wampapp.session.call('com.example.square', x)
+   returnValue('{} square is {}'.format(x, result))
+
+
+@webapp.route('/squareform', methods = ['POST'])
+@inlineCallbacks
+def squareform(request):
+   x = int(request.args.get('x', [0])[0])
+   result = yield wampapp.session.call('com.example.square', x)
+   page = webapp.templates.get_template('result.html')
+   content = page.render(x = x, result = result)
+   returnValue(content)
+
+
+
+if __name__ == "__main__":
+   import sys
+   from twisted.python import log
+   from twisted.web.server import Site
+   from twisted.internet import reactor
+   log.startLogging(sys.stdout)
+
+   reactor.listenTCP(8090, Site(webapp.resource()))
+   wampapp.run("ws://localhost:9000", "realm1", standalone = True)
