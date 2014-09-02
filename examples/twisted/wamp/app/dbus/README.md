c1ad138fa38363cbe0faa761597b1338172ac41f
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-09-02 18:43:42 +0200

    migrate example to WAMP2

--- examples/twisted/wamp/app/dbus/README.md
+++ examples/twisted/wamp/app/dbus/README.md
@@ -1,58 +1,42 @@
-Trigger DBus Desktop Notifications via WebSocket/WAMP
-=====================================================
+# Trigger DBus Desktop Notifications via WAMP
 
-This example shows how to bridge WebSocket/WAMP and DBus.
+This example shows how to bridge WAMP and DBus.
 
-The example allows to trigger Linux desktop notifications on one or multiple machines
-from a browser based UI running anywhere via a WebSocket/WAMP PubSub
-message broker in between.
+The example allows to trigger Linux desktop notifications by publishing to a WAMP topic. This allows
+to show Linux desktop notifications on any number of Linux desktops. Kind of alert system.
 
-For DBus support, we will use [txdbus](https://github.com/cocagne/txdbus),
-a new native Python DBus binding for Twisted which does not depend on the glib
-reactor or libdbus Python bindings.
-
-The example consists of 3 parts:
-
-  * client.py
-  * server.py
-  * index.html
-
-The **client.py** runs on a Linux desktop  and subscribes to 2 PubSub topics:
-
- * user specific topic
- * the "all" topic (for notifications to all)
-
-Upon receiving an event over WAMP for one of above topics, a desktop notification is triggered via *txdbus*.
-
-The **server.py** runs on an arbitrary machine and provides the PubSub message brokering. It also provides an embedded web server for the Web UI.
-
-The **index.html** is the Web UI used to trigger desktop notifications. It is a WAMP client using AutobahnJS and publishes notification events to topics for the **server.py** to forward to connected Linux desktops.
+> Note: For DBus support, we will use [txdbus](https://github.com/cocagne/txdbus), a native
+Python DBus binding for Twisted which does not depend on the glib reactor or libdbus Python bindings.
 
 
-Running
--------
+## Prerequisites
 
-Run the server by doing
+You'll need **txdbus** installed:
 
-    python server.py debug
+    pip install txdbus
+
+
+## Running on a single Host
+
+Run the bridge with an embedded WAMP router
+
+    python bridge.py
 
 and open
 
-    http://<Server IP>:8080/
+    http://localhost:8080/
 
 in your browser.
 
-Open a terminal on a Linux desktop and
 
-	python client.py ws://<Server IP>:9000 user1 secret
+## Running on multiples Hosts
 
-Optionally, open a second terminal on another Linux desktop and
+On each host, start the bridge connecting to a central WAMP router
 
-	python client.py ws://<Server IP>:9000 user2 geheim
+    python bridge.py --router ws://myrouter.com
 
-Now, from the Web UI, send desktop notifications to
+and open
 
- * all
- * user1
- * user2
+    http://localhost:8080/
 
+in your browser on *any* host to send a notification that will pop up on *all* hosts.
---
+++ examples/twisted/wamp/app/dbus/bridge.py
@@ -0,0 +1,124 @@
+###############################################################################
+##
+##  Copyright (C) 2012-2014 Tavendo GmbH
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##
+###############################################################################
+
+from twisted.internet.defer import inlineCallbacks
+
+from txdbus import error, client
+
+from autobahn import wamp
+from autobahn.twisted.wamp import ApplicationSession
+from autobahn.twisted.util import sleep
+
+
+class DbusNotifier(ApplicationSession):
+
+   @inlineCallbacks
+   def onJoin(self, details):
+
+      conn = yield client.connect(reactor, 'session')
+      print("Dbus connection created")
+
+      self._notifier = yield conn.getRemoteObject('org.freedesktop.Notifications',
+         '/org/freedesktop/Notifications')
+      print("Dbus notification object created")
+
+      yield self.subscribe(self)
+      print("Dbus notifier subscribed")
+
+
+   @inlineCallbacks
+   @wamp.subscribe(u'com.example.dbus.on_notify')
+   def onNotify(self, title, body, duration = 2):
+
+      print("Notification received: title = '{}', body = '{}', duration = {}".format(title, body, duration))
+
+      try:
+         nid = yield self._notifier.callRemote('Notify',
+            'WAMP/Dbus Notifier',
+            0,
+            '',
+            str(title),
+            str(body),
+            [],
+            {},
+            1)
+
+         print("Desktop notification shown (NID = {}).. closing in {} seconds".format(nid, duration))
+
+         yield sleep(duration)
+         print("Closing notification")
+
+         yield self._notifier.callRemote('CloseNotification', nid)
+         print("Notification closed")
+
+      except Exception as e:
+         print("ERROR: {}".format(e))
+
+
+
+if __name__ == '__main__':
+
+   import sys, argparse
+
+   ## parse command line arguments
+   ##
+   parser = argparse.ArgumentParser()
+
+   parser.add_argument("-d", "--debug", action = "store_true",
+                       help = "Enable debug output.")
+
+   parser.add_argument("--web", type = int, default = 8080,
+                       help = 'Web port to use for embedded Web server. Use 0 to disable.')
+
+   parser.add_argument("--router", type = str, default = None,
+                       help = 'If given, connect to this WAMP router. Else run an embedded router on 9000.')
+
+   args = parser.parse_args()
+
+   if args.debug:
+      from twisted.python import log
+      log.startLogging(sys.stdout)     
+
+   ## import Twisted reactor
+   ##
+   from twisted.internet import reactor
+   print("Using Twisted reactor {0}".format(reactor.__class__))
+
+
+   ## create embedded web server for static files
+   ##
+   if args.web:
+      from twisted.web.server import Site
+      from twisted.web.static import File
+      reactor.listenTCP(args.web, Site(File(".")))
+
+
+   ## run WAMP application component
+   ##
+   from autobahn.twisted.wamp import ApplicationRunner
+   router = args.router or 'ws://localhost:9000'
+
+   runner = ApplicationRunner(router, u"realm1", standalone = not args.router,
+      debug = False,             # low-level logging
+      debug_wamp = args.debug,   # WAMP level logging
+      debug_app = args.debug     # app-level logging
+   )
+
+   ## start the component and the Twisted reactor ..
+   ##
+   runner.run(DbusNotifier)
