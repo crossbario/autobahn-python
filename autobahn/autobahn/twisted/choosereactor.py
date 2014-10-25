42e841795e3e8af319ed9bf5c2693af5e130d25a
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-10-25 18:03:45 +0200

    improve install_optimal_reactor; helps with PyInstaller woes ..

--- autobahn/autobahn/twisted/choosereactor.py
+++ autobahn/autobahn/twisted/choosereactor.py
@@ -22,61 +22,85 @@ __all__ = (
 )
 
 
-def install_optimal_reactor():
+def install_optimal_reactor(verbose = False):
    """
    Try to install the optimal Twisted reactor for platform.
+
+   :param verbose: If ``True``, print what happens.
+   :type verbose: bool
    """
    import sys
+   from twisted.python import reflect
 
+   ## determine currently installed reactor, if any
+   ##
+   if 'twisted.internet.reactor' in sys.modules:
+      current_reactor = reflect.qual(sys.modules['twisted.internet.reactor'].__class__).split('.')[-1]
+   else:
+      current_reactor = None
+
+   ## depending on platform, install optimal reactor
+   ##
    if 'bsd' in sys.platform or sys.platform.startswith('darwin'):
+
+      ## *BSD and MacOSX
+      ##
+      if current_reactor != 'KQueueReactor':
+         try:
+            v = sys.version_info
+            if v[0] == 1 or (v[0] == 2 and v[1] < 6) or (v[0] == 2 and v[1] == 6 and v[2] < 5):
+               raise Exception("Python version too old ({0}) to use kqueue reactor".format(sys.version))
+            from twisted.internet import kqreactor
+            kqreactor.install()
+         except Exception as e:
+            print("WARNING: Running on *BSD or MacOSX, but cannot install kqueue Twisted reactor ({0}).".format(e))            
+         else:
+            if verbose:
+               print("Running on *BSD or MacOSX and optimal reactor (kqueue) was installed.")
+      else:
+         if verbose:
+            print("Running on *BSD or MacOSX and optimal reactor (kqueue) already installed.")
+
+   elif sys.platform in ['win32']:
+
+      ## Windows
+      ##
+      if current_reactor != 'IOCPReactor':
+         try:
+            from twisted.internet.iocpreactor import reactor as iocpreactor
+            iocpreactor.install()
+         except Exception as e:
+            print("WARNING: Running on Windows, but cannot install IOCP Twisted reactor ({0}).".format(e))
+         else:
+            if verbose:
+               print("Running on Windows and optimal reactor (ICOP) was installed.")
+      else:
+         if verbose:
+            print("Running on Windows and optimal reactor (ICOP) already installed.")
+
+   elif sys.platform.startswith('linux'):
+
+      ## Linux
+      ##
+      if current_reactor != 'EPollReactor':
+         try:
+            from twisted.internet import epollreactor
+            epollreactor.install()
+         except Exception as e:
+            print("WARNING: Running on Linux, but cannot install Epoll Twisted reactor ({0}).".format(e))
+         else:
+            if verbose:
+               print("Running on Linux and optimal reactor (epoll) was installed.")
+      else:
+         if verbose:
+            print("Running on Linux and optimal reactor (epoll) already installed.")
+
+   else:
       try:
-         v = sys.version_info
-         if v[0] == 1 or (v[0] == 2 and v[1] < 6) or (v[0] == 2 and v[1] == 6 and v[2] < 5):
-            raise Exception("Python version too old (%s)" % sys.version)
-         from twisted.internet import kqreactor
-         kqreactor.install()
+         from twisted.internet import default as defaultreactor
+         defaultreactor.install()
       except Exception as e:
-         print("""
-   WARNING: Running on BSD or Darwin, but cannot use kqueue Twisted reactor.
-
-    => %s
-
-   To use the kqueue Twisted reactor, you will need:
-
-     1. Python >= 2.6.5 or PyPy > 1.8
-     2. Twisted > 12.0
-
-   Note the use of >= and >.
-
-   Will let Twisted choose a default reactor (potential performance degradation).
-   """ % str(e))
-         pass
-
-   if sys.platform in ['win32']:
-      try:
-         from twisted.application.reactors import installReactor
-         installReactor("iocp")
-      except Exception as e:
-         print("""
-   WARNING: Running on Windows, but cannot use IOCP Twisted reactor.
-
-    => %s
-
-   Will let Twisted choose a default reactor (potential performance degradation).
-   """ % str(e))
-
-   if sys.platform.startswith('linux'):
-      try:
-         from twisted.internet import epollreactor
-         epollreactor.install()
-      except Exception as e:
-         print("""
-   WARNING: Running on Linux, but cannot use Epoll Twisted reactor.
-
-    => %s
-
-   Will let Twisted choose a default reactor (potential performance degradation).
-   """ % str(e))
+         print("WARNING: Could not install default Twisted reactor for this platform ({0}).".format(e))
 
 
 
@@ -106,7 +130,7 @@ def install_reactor(explicitReactor = None, verbose = False):
       ##
       if verbose:
          print("Automatically choosing optimal Twisted reactor")
-      install_optimal_reactor()
+      install_optimal_reactor(verbose)
 
    ## now the reactor is installed, import it
    from twisted.internet import reactor
