cdb0c321ca99be77a7123988e90de14848e72d74
meejah <meejah@meejah.ca>
2015-06-17 14:51:15 -0600

    fix spacing and code on overview example

--- examples/twisted/wamp/overview/backend.py
+++ examples/twisted/wamp/overview/backend.py
@@ -5,20 +5,20 @@ from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 # or: from autobahn.asyncio.wamp import ApplicationSession
 
 class MyComponent(ApplicationSession):
-   @inlineCallbacks
-   def onJoin(self, details):
-       # publish an event every second. The event payloads can be
-       # anything JSON- and msgpack- serializable
-       def publish():
-           return self.publish('com.myapp.hello', 'Hello, world!')
-       LoopingCall(publish).start(1)
+    @inlineCallbacks
+    def onJoin(self, details):
+        # publish an event every second. The event payloads can be
+        # anything JSON- and msgpack- serializable
+        def publish():
+            return self.publish('com.myapp.hello', 'Hello, world!')
+        LoopingCall(publish).start(1)
 
-       # a remote procedure; see frontend.py for a Python front-end
-       # that calls this. Any language with WAMP bindings can now call
-       # this procedure if its connected to the same router and realm.
-       def add2(x, y):
-           return x + y
-       yield self.register(add2, 'com.myapp.add2');
+        # a remote procedure; see frontend.py for a Python front-end
+        # that calls this. Any language with WAMP bindings can now call
+        # this procedure if its connected to the same router and realm.
+        def add2(x, y):
+            return x + y
+        yield self.register(add2, 'com.myapp.add2');
 
 
 if __name__ == '__main__':
--- examples/twisted/wamp/overview/frontend.py
+++ examples/twisted/wamp/overview/frontend.py
@@ -4,17 +4,17 @@ from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 # or: from autobahn.asyncio.wamp import ApplicationSession
 
 class MyComponent(ApplicationSession):
-   @inlineCallbacks
-   def onJoin(self, details):
-       # listening for the corresponding message from the "backend"
-       # (any session that .publish()es to this topic).
-       def onevent(msg):
-           print("Got event: {}".format(msg))
-       yield self.subscribe(onevent, 'com.myapp.hello')
+    @inlineCallbacks
+    def onJoin(self, details):
+        # listening for the corresponding message from the "backend"
+        # (any session that .publish()es to this topic).
+        def onevent(msg):
+            print("Got event: {}".format(msg))
+        yield self.subscribe(onevent, 'com.myapp.hello')
 
-       # call a remote procedure.
-       res = yield self.call('com.myapp.add2', 2, 3)
-       print("Got result: {}".format(res))
+        # call a remote procedure.
+        res = yield self.call('com.myapp.add2', 2, 3)
+        print("Got result: {}".format(res))
 
 
 if __name__ == '__main__':
