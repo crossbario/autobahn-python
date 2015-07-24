259850a60073b68057300571b141a2b3d1ac5da3
HawkOwl <hawkowl@atleastfornow.net>
2015-07-24 14:18:19 +0800

    kwarg instead

--- autobahn/logger.py
+++ autobahn/logger.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 
-def make_logger(logger_type):
+def make_logger(logger_type=None):
     if logger_type == "twisted":
         # If we've been asked for the Twisted logger, try and get the new one
         try:
