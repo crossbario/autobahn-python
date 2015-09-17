34a663810d36048c2d67c410b9ed3028f0581bac
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-09-17 19:25:20 +0200

    polish up

--- examples/twisted/wamp/test_newapi_short.py
+++ examples/twisted/wamp/test_newapi_short.py
@@ -1,14 +1,24 @@
+from twisted.internet.defer import inlineCallbacks as coroutine
 from autobahn.twisted import run
 
-
+@coroutine
 def on_join(session):
-    print('session connected: {}'.format(session))
-    session.leave()
-
+    # the session has joined the realm and the full range of
+    # WAMP interactions are now possible
+    try:
+        res = yield session.call(u'com.example.add2', 2, 3)
+        print("Result: {}".format(res))
+    except Exception as e:
+        print("Error: {}".format(e))
+    finally:
+        session.leave()
 
 def main(connection):
-    connection.session.on_join(on_join)
-
+    # this is hooking into a session event, but that is
+    # replicated on as a connection event
+    connection.on_join(on_join)
 
 if __name__ == '__main__':
+    # this is using the defaults: WAMP-over-WebSocket
+    # to "ws://127.0.0.1:8080/ws" and realm "public"
     return run(main)
