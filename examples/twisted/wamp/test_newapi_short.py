ac445c39d1210d41baa0c6a9b6d747c73c1da8c0
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-09-17 20:50:27 +0200

    more new api example code

--- examples/twisted/wamp/test_newapi_short.py
+++ examples/twisted/wamp/test_newapi_short.py
@@ -1,5 +1,5 @@
 from twisted.internet.defer import inlineCallbacks as coroutine
-from autobahn.twisted import Runner
+from autobahn.twisted import Client
 
 
 @coroutine
@@ -14,11 +14,15 @@ def on_join(session):
 
 
 if __name__ == '__main__':
-    # this is Runner, a high-level API above Connection and Session
-    runner = Runner()
+    # this is Client, a high-level API above Connection and Session
+    # it's a what is nowerdays ApplicationRunner, but with a better
+    # name and a listener based interface
+    client = Client()
 
     # "on_join" is a Session event that bubbled up via Connection
-    # to Runner here. this works since Connection/Session have default
-    # implementations that work with WAMP defaults
-    runner.on_join(on_join)
-    runner.run()
+    # to Client here. this works since Connection/Session have default
+    # implementations that by using WAMP defaults
+    client.on_join(on_join)
+
+    # now make it run ..
+    client.run()
