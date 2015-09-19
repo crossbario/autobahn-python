9fb67e72718038017f37a09839171e35fca8c2ce
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-09-19 23:49:36 +0200

    polish

--- examples/twisted/wamp/test_newapi3.py
+++ examples/twisted/wamp/test_newapi3.py
@@ -1,18 +1,26 @@
 from twisted.internet.task import react
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks as coroutine
 from autobahn.twisted.connection import Connection
 
 
 def main(reactor, connection):
 
+    @coroutine
     def on_join(session, details):
-        print("on_join", details)
+        print("on_join: {}".format(details))
+
+        def add2(a, b):
+            return a + b
+
+        yield self.register(add2, u'com.example.add2')
+
         try:
-            res = yield self.call(u'com.example.add2', [2, 3])
-            print(res)
+            res = yield self.call(u'com.example.add2', 2, 3)
+            print("result: {}".format(res))
         except Exception as e:
-            print(e)
-        self.leave()
+            print("error: {}".format(e))
+        finally:
+            self.leave()
 
     connection.on('join', on_join)
 
