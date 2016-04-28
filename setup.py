5f2b39664387a7aebb48a8db087013c4888b77c9
meejah <meejah@meejah.ca>
2016-04-28 09:50:11 -0600

    bump txaio requirement

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ setup(
     platforms='Any',
     install_requires=[
         'six>=1.10.0',      # MIT license
-        'txaio>=2.3.1',     # MIT license
+        'txaio>=2.5.0',     # MIT license
     ],
     extras_require={
         'all': extras_require_all,
