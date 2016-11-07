b6a224ae642e7d16cc71688bb8661db54bfef052
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-11-07 12:20:15 +0100

    copyrights transferred from Tavendo to Crossbar.io Technologies

--- autobahn/twisted/test/test_rawsocket.py
+++ autobahn/twisted/test/test_rawsocket.py
@@ -2,7 +2,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) Tavendo GmbH
+# Copyright (c) Crossbar.io Technologies GmbH
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
