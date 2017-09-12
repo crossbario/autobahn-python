aebaedc46b5944fe56b3f94aa06c39e3fcfc98db
meejah <meejah@meejah.ca>
2017-09-12 09:27:09 -0600

    wire data must be bytes

--- autobahn/websocket/protocol.py
+++ autobahn/websocket/protocol.py
@@ -3362,9 +3362,9 @@ class WebSocketClientProtocol(WebSocketProtocol):
         """
         # construct proxy connect HTTP request
         #
-        request = "CONNECT %s:%d HTTP/1.1\x0d\x0a" % (self.factory.host.encode("utf-8"), self.factory.port)
-        request += "Host: %s:%d\x0d\x0a" % (self.factory.host.encode("utf-8"), self.factory.port)
-        request += "\x0d\x0a"
+        request = b"CONNECT %s:%d HTTP/1.1\x0d\x0a" % (self.factory.host.encode("utf-8"), self.factory.port)
+        request += b"Host: %s:%d\x0d\x0a" % (self.factory.host.encode("utf-8"), self.factory.port)
+        request += b"\x0d\x0a"
 
         self.log.debug("{request}", request=request)
 
