f029ff58503903f2f9fb2e23486e0a1c43787873
Omer Akram <om26er@gmail.com>
2017-09-23 22:31:23 +0500

    Close aio event loop on exit (#904)

--- autobahn/asyncio/component.py
+++ autobahn/asyncio/component.py
@@ -482,3 +482,7 @@ def run(components, log_level='info'):
     # finally:
     #     signal.signal(signal.SIGINT, signal.SIG_DFL)
     #     signal.signal(signal.SIGTERM, signal.SIG_DFL)
+
+    # Close the event loop at the end, otherwise an exception is
+    # thrown. https://bugs.python.org/issue23548
+    loop.close()
