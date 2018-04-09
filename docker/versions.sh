039e224bf3ea65ec7fe919ec7f966d8fd6e28f45
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-04-09 15:34:56 +0200

    moved from crossbar-docker (#982)

---
+++ docker/versions.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#
+# CHANGE FOR NEW RELEASES (these need to be proper Git tags in the respective repo):
+#
+export AUTOBAHN_PYTHON_VERSION='18.3.1'
+#
+# END OF CONFIG
+#
+
+#
+# Git working directories of all relevant repos must reside
+# in parallel (as siblings) to this repository
+#
+export AUTOBAHN_PYTHON_VCS_REF=`git --git-dir="../.git" rev-list -n 1 v${AUTOBAHN_PYTHON_VERSION} --abbrev-commit`
+export BUILD_DATE=`date -u +"%Y-%m-%d"`
+
+echo ""
+echo "The Crossbar.io Project (build date ${BUILD_DATE})"
+echo ""
+echo "autobahn-python ${AUTOBAHN_PYTHON_VERSION} [${AUTOBAHN_PYTHON_VCS_REF}]"
+echo ""
---
+++ docker/x86_64/app/README.txt
@@ -0,0 +1,5 @@
+Put your microservice code and assets in here.
+
+The container will automatically start the Python script
+
+    python client.py --realm $CBREALM --url $CBURL
