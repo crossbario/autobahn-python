2bab51f31fd3254ef981a5bf8dfd3cd2c3461e0e
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-09-02 22:00:24 +0200

    release

--- docker/versions.sh
+++ docker/versions.sh
@@ -3,7 +3,7 @@
 #
 # CHANGE FOR NEW RELEASES (these need to be proper Git tags in the respective repo):
 #
-export AUTOBAHN_PYTHON_VERSION='18.8.1'
+export AUTOBAHN_PYTHON_VERSION='18.9.1'
 #
 # END OF CONFIG
 #
