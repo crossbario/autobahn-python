9d65b508cc108730b4b6a74ba35afe0fa1d5ffca
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-02-22 10:59:23 +0100

    deploy wheel to s3 (#1121)

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,10 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-# build the docs, source package and binary (executable). this will produce:
-#
-#  - $HOME/crossbar-docs
-#
-# upload to "crossbar.io" company S3 bucket
+export AWS_DEFAULT_REGION=eu-central-1
+export AWS_S3_BUCKET_NAME=crossbarbuilder
+# AWS_ACCESS_KEY_ID         : must be set in Travis CI build context
+# AWS_SECRET_ACCESS_KEY     : must be set in Travis CI build context
+
+set -ev
+
+# TRAVIS_BRANCH, TRAVIS_PULL_REQUEST, TRAVIS_TAG
+
+# PR => don't deploy and exit
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+    echo '[1] deploy script called for PR - exiting ..';
+    exit 0;
+
+# direct push to master => deploy
+elif [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    echo '[2] deploy script called for direct push to master: continuing to deploy!';
+
+# tagged release => deploy
+elif [ -n "$TRAVIS_TAG" ]; then
+    echo '[3] deploy script called for tagged release: continuing to deploy!';
+
+# outside travis? => deploy
+else
+    echo '[?] deploy script called outside Travis? continuing to deploy!';
+
+fi
 
 # only show number of env vars .. should be 4 on master branch!
 # https://docs.travis-ci.com/user/pull-requests/#Pull-Requests-and-Security-Restrictions
@@ -17,9 +39,18 @@ echo 'installing aws tools ..'
 pip install awscli
 which aws
 aws --version
-aws s3 ls ${AWS_S3_BUCKET_NAME}
+aws s3 ls ${AWS_S3_BUCKET_NAME}/wheels/
+
+# build python source dist and wheels
+echo 'building package ..'
+python setup.py sdist bdist_wheel --universal
+ls -la ./dist
+
+# upload to S3: https://s3.eu-central-1.amazonaws.com/crossbarbuilder/wheels/
+echo 'uploading package ..'
+aws s3 cp --recursive ./dist s3://${AWS_S3_BUCKET_NAME}/wheels
 
 # build and deploy latest docs
-echo 'building and uploading docs ..'
-tox -c tox.ini -e sphinx
-aws s3 cp --recursive --acl public-read ${HOME}/crossbar-docs s3://${AWS_S3_BUCKET_NAME}/docs-latest
+#echo 'building and uploading docs ..'
+#tox -c tox.ini -e sphinx
+#aws s3 cp --recursive --acl public-read ${HOME}/crossbar-docs s3://${AWS_S3_BUCKET_NAME}/docs-latest
