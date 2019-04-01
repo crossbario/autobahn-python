2cce98a41b9ae32870f950df3072d740d2c75548
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-04-01 12:30:49 +0200

    deactive for now

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -53,8 +53,10 @@ aws s3 cp --recursive ./dist s3://${AWS_S3_BUCKET_NAME}/wheels
 
 # tell crossbar-builder about this new wheel push
 # get 'wamp' command, always with latest autobahn master
-pip install https://github.com/crossbario/autobahn-python/archive/master.zip#egg=autobahn[twisted]
-wamp --authid wheel_pusher --url ws://office2dmz.crossbario.com:8008/ --realm webhook call builder.wheel_pushed --keyword name autobahn-python --keyword publish true
+
+# currently does not work: https://github.com/crossbario/autobahn-python/issues/1163
+# pip install https://github.com/crossbario/autobahn-python/archive/master.zip#egg=autobahn[twisted]
+# wamp --authid wheel_pusher --url ws://office2dmz.crossbario.com:8008/ --realm webhook call builder.wheel_pushed --keyword name autobahn-python --keyword publish true
 
 # build and deploy latest docs
 #echo 'building and uploading docs ..'
