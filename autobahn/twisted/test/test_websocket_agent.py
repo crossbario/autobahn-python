58c2e3395906ef339fd690f49d69095c8c337845
meejah <meejah@meejah.ca>
2019-04-24 00:45:27 -0600

    refactor; echo server example works

--- autobahn/twisted/test/test_websocket_agent.py
+++ autobahn/twisted/test/test_websocket_agent.py
@@ -8,66 +8,23 @@ from autobahn.twisted.websocket import WebSocketClientProtocol
 from twisted.internet import reactor
 
 
-class SpammingWebSocketServerProtocol(WebSocketServerProtocol):
-    def onMessage(self, *args, **kw):
-        print("SERVER MESSAGE: {} {}".format(args, kw))
-
-
-class LoggingWebSocketServerProtocol(WebSocketServerProtocol):
-
-    messages = []
-    reactor = None
-
-    def onOpen(self):
-        # "break the loop" in synchonous agent behavior that means
-        # we'll "do" all the message-sending before anything gets a
-        # chance to listn ... but XXX THINK do we actually need to do
-        # this "in autobahn" or is this a produce of "all in-memory
-        # transports" which take zero time?
-        def send_messages():
-            for msg in self.messages:
-                print("sending {}".format(msg))
-                self.sendMessage(msg)
-        self.reactor.callLater(0, send_messages)
-
-    def onMessage(self, *args, **kw):
-        print("SERVER GOT MESSAGE: {} {}".format(args, kw))
-
-
 class TestAgent(unittest.TestCase):
 
     def setUp(self):
         self.reactor = MemoryReactorClockResolver()
 
     @inlineCallbacks
-    def _test_foo(self):
-        agent = create_memory_agent(self.reactor, SpammingWebSocketServerProtocol)
-        proto = yield agent.open("ws://localhost:1234/ws", dict())
-
-        proto.sendMessage(b"hello")
-        agent.flush()
-
-    @inlineCallbacks
     def test_echo_server(self):
 
         class EchoServer(WebSocketServerProtocol):
-            def onOpen(self):
-                print("open")
-                print("sent",self.sendMessage(b"hello"))
-
-            def onMesssage(self, msg, isBinary):
-                print("gotmessage {}".format(msg))
-                self.sendMessage(msg, isBinary=is_binary)
-
-            def onClose(*args):
-                print("close {}".format(args))
+            def onMessage(self, msg, is_binary):
+                self.sendMessage(msg)
 
         agent = create_memory_agent(self.reactor, EchoServer)
         proto = yield agent.open("ws://localhost:1234/ws", dict())
 
         messages = []
-        def got(msg, isBinary):
-            print("got", msg)
+        def got(msg, is_binary):
             messages.append(msg)
         proto.on("message", got)
 
