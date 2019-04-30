5b40dce955273d7ffe3d2ffecb34512210a05010
meejah <meejah@meejah.ca>
2019-04-30 01:17:12 -0600

    might have no handshake data

--- autobahn/twisted/util.py
+++ autobahn/twisted/util.py
@@ -119,6 +119,12 @@ def transport_channel_id(transport, is_server, channel_id_type):
             # to the router (=server)
             tls_finished_msg = transport._tlsConnection.get_finished()
 
+        if tls_finished_msg is None:
+            return None
+            raise RuntimeError(
+                "A TLS connection was attempted, but no Finished message found"
+            )
+
         m = hashlib.sha256()
         m.update(tls_finished_msg)
         return m.digest()
