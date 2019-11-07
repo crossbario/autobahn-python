d7272c7a949c626bbaba023bcdeaea338572208a
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-11-07 14:04:53 +0100

    sync

--- autobahn/xbr/test/test_mnemonic.py
+++ autobahn/xbr/test/test_mnemonic.py
@@ -59,7 +59,7 @@ if HAS_XBR:
         ('0x3fD652C93dFA333979ad762Cf581Df89BaBa6795', '0xae9a2e131e9b359b198fa280de53ddbe2247730b881faae7af08e567e58915bd'),
     ]
 
-    @unittest.skip(six.PY2, 'XBR needs Python 3')
+    @unittest.skip(six.PY2)
     class TestEthereumMnemonic(unittest.TestCase):
 
         def test_check_seedphrase(self):
