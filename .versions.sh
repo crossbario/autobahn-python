8204396049f8907cc666e9f8148c22246a19011e
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-04-17 16:17:39 +0200

    more logging; fix version.sh (#1363)

--- .versions.sh
+++ .versions.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-export AUTOBAHN_VERSION=$(grep -E '^(__version__)' ./autobahn/_version.py | cut -d ' ' -f3 | sed -e 's|[u"'\'']||g')
-export AUTOBAHN_VCS_REF=`git --git-dir="./.git" rev-list -n 1 v${AUTOBAHN_VERSION} --abbrev-commit`
+export AUTOBAHN_PYTHON_VERSION=$(grep -E '^(__version__)' ./autobahn/_version.py | cut -d ' ' -f3 | sed -e 's|[u"'\'']||g')
+export AUTOBAHN_PYTHON_VCS_REF=`git --git-dir="./.git" rev-list -n 1 v${AUTOBAHN_PYTHON_VERSION} --abbrev-commit`
 export BUILD_DATE=`date -u +"%Y-%m-%d"`
 
 echo ""
 echo "Build environment configured:"
 echo ""
-echo "  AUTOBAHN_VERSION = ${AUTOBAHN_VERSION}"
-echo "  AUTOBAHN_VCS_REF = ${AUTOBAHN_VCS_REF}"
+echo "  AUTOBAHN_PYTHON_VERSION = ${AUTOBAHN_PYTHON_VERSION}"
+echo "  AUTOBAHN_PYTHON_VCS_REF = ${AUTOBAHN_PYTHON_VCS_REF}"
 echo "  BUILD_DATE       = ${BUILD_DATE}"
 echo ""
