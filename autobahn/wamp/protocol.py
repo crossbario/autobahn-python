b73625ce5fda478ea3ec038e44095fe3488aa3f7
Omer Akram <om26er@gmail.com>
2020-07-20 03:36:08 +0500

    fix 1322

--- autobahn/wamp/protocol.py
+++ autobahn/wamp/protocol.py
@@ -502,10 +502,14 @@ class ApplicationSession(BaseSession):
             response = []
             for name, kind in func.__annotations__.items():
                 if name in arguments:
+                    # if a "type" has __origin__ attribute it is a
+                    # parameterized generic
                     if getattr(kind, "__origin__", None) == Union:
+                        acceptable_types = [arg.__name__ for arg in kind.__args__]
                         if not isinstance(arguments[name], kind.__args__):
                             response.append(
-                                "'{}' required={} got={}".format(name, kind.__name__, type(arguments[name]).__name__))
+                                "'{}' acceptable types={} got={}".format(name, acceptable_types,
+                                                                         type(arguments[name]).__name__))
                     elif not isinstance(arguments[name], kind):
                         response.append(
                             "'{}' required={} got={}".format(name, kind.__name__, type(arguments[name]).__name__))
