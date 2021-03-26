93c4cdfe9420486ed70115e0d77bbc2c019ded6f
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-03-26 08:14:05 +0100

    fix whitespace

--- autobahn/xbr/_wallet.py
+++ autobahn/xbr/_wallet.py
@@ -30,53 +30,53 @@ import argon2
 
 
 def stretch_argon2_secret(email: str, password: str, salt: Optional[bytes] = None) -> bytes:
-	"""
-	Compute argon2id based secret from user email and password only. This uses Argon2id
-	for stretching a potentially weak user password/PIN and subsequent HKDF based key
-	extending to derive private key material (PKM) for different usage contexts.
+    """
+    Compute argon2id based secret from user email and password only. This uses Argon2id
+    for stretching a potentially weak user password/PIN and subsequent HKDF based key
+    extending to derive private key material (PKM) for different usage contexts.
 
-	The Argon2 parameters used are the following:
+    The Argon2 parameters used are the following:
 
-	* kdf ``argon2id-13``
-	* time cost ``4096``
-	* memory cost ``512``
-	* parallelism ``1``
+    * kdf ``argon2id-13``
+    * time cost ``4096``
+    * memory cost ``512``
+    * parallelism ``1``
 
-	See `draft-irtf-cfrg-argon2 <https://datatracker.ietf.org/doc/draft-irtf-cfrg-argon2/>`__ and
-	`argon2-cffi <https://argon2-cffi.readthedocs.io/en/stable/>`__.
+    See `draft-irtf-cfrg-argon2 <https://datatracker.ietf.org/doc/draft-irtf-cfrg-argon2/>`__ and
+    `argon2-cffi <https://argon2-cffi.readthedocs.io/en/stable/>`__.
 
-	:param email: User email.
-	:param password: User password.
-	:param salt: Optional salt to use (must be 16 bytes long). If none is given, compute salt
-		from email as ``salt = SHA256(email)[:16]``.
-	:return: The computed private key material (256b, 32 octets).
-	"""
-	if not salt:
-		m = hashlib.sha256()
-		m.update(email.encode('utf8'))
-		salt = m.digest()[:16]
-	assert len(salt) == 16
+    :param email: User email.
+    :param password: User password.
+    :param salt: Optional salt to use (must be 16 bytes long). If none is given, compute salt
+        from email as ``salt = SHA256(email)[:16]``.
+    :return: The computed private key material (256b, 32 octets).
+    """
+    if not salt:
+        m = hashlib.sha256()
+        m.update(email.encode('utf8'))
+        salt = m.digest()[:16]
+    assert len(salt) == 16
 
-	pkm = argon2.low_level.hash_secret_raw(
-		secret=password.encode('utf8'),
-		salt=salt,
-		time_cost=4096,
-		memory_cost=512,
-		parallelism=1,
-		hash_len=32,
-		type=argon2.low_level.Type.ID,
-		version=19,
-	)
+    pkm = argon2.low_level.hash_secret_raw(
+        secret=password.encode('utf8'),
+        salt=salt,
+        time_cost=4096,
+        memory_cost=512,
+        parallelism=1,
+        hash_len=32,
+        type=argon2.low_level.Type.ID,
+        version=19,
+    )
 
-	return pkm
+    return pkm
 
 
 def expand_argon2_secret(pkm: bytes, context: str, salt: Optional[bytes] = None) -> bytes:
-	"""
+    """
 
-	:param pkm:
-	:param context:
-	:param salt:
-	:return:
-	"""
-	pass
+    :param pkm:
+    :param context:
+    :param salt:
+    :return:
+    """
+    pass
