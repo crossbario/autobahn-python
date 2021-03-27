29726a219b5c00df5ec005b76f179e45b0cc4616
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-03-27 11:53:02 +0100

    sync

--- examples/xbr/test_onboarding_ui.py
+++ examples/xbr/test_onboarding_ui.py
@@ -2,6 +2,7 @@
 # https://twistedmatrix.com/documents/current/core/howto/choosing-reactor.html
 
 import os
+import sys
 
 import gi
 
@@ -11,21 +12,18 @@ from gi.repository import Gtk
 from twisted.internet import gtk3reactor
 gtk3reactor.install()
 
-from twisted.internet import reactor
-
 import txaio
 txaio.use_twisted()
 
 import click
 
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.task import react
 
 from autobahn.xbr._config import UserConfig
 from autobahn.xbr import generate_seedphrase
 
 
-
-class SelectNewProfile(Gtk.Window):
+class SelectNewProfile(Gtk.Assistant):
     """
     local user profile config there?
     if no, show:
@@ -45,16 +43,20 @@ class SelectNewProfile(Gtk.Window):
     SELECTED_RECOVER = 3
 
     def __init__(self, config_path, profile_name, selected):
-        Gtk.Window.__init__(self, title='Register with XBR network')
+        Gtk.Assistant.__init__(self)
 
-        self.set_resizable(False)
+        self.set_title("XBR Network")
         self.set_default_size(600, 600)
         self.set_border_width(50)
+        self.set_resizable(False)
 
-        notebook1 = Gtk.Notebook()
-        notebook1.set_show_tabs(False)
-        self.add(notebook1)
+        self.connect("cancel", self.on_cancel_clicked)
+        self.connect("close", self.on_close_clicked)
+        self.connect("apply", self.on_apply_clicked)
 
+        ##
+        ## page 1: select top-level action
+        ##
         grid1 = Gtk.Grid()
         grid1.set_row_spacing(20)
         grid1.set_column_spacing(20)
@@ -62,30 +64,34 @@ class SelectNewProfile(Gtk.Window):
         grid1.set_margin_bottom(20)
         grid1.set_margin_start(20)
         grid1.set_margin_end(20)
-        self.add(grid1)
+
+        image1 = Gtk.Image()
+        image1.set_from_file('xbr_white.svg')
+        grid1.attach(image1, 0, 0, 2, 1)
 
         label0 = Gtk.Label(label='Initial configuration, please select:\n')
-        label0.set_alignment(0.5, 0)
-        grid1.attach(label0, 0, 0, 2, 1)
+        label0.set_alignment(0, 0)
+        grid1.attach(label0, 0, 1, 1, 1)
 
-        label1 = Gtk.Label(label='Create a new account')
+        label1 = Gtk.Label(label='Create a new account or start from fresh')
         label1.set_alignment(0, 0.5)
         label1.set_justify(Gtk.Justification.LEFT)
-        grid1.attach(label1, 1, 1, 1, 1)
+        grid1.attach(label1, 1, 2, 1, 1)
 
         button1 = Gtk.Button.new_with_label('New account')
         def on_button1(res):
             self.log.info('SELECTED_NEW: {res}', res=res)
-            self.destroy()
-            txaio.resolve(selected, self.SELECTED_NEW)
+            # self.destroy()
+            # txaio.resolve(selected, self.SELECTED_NEW)
+            self.set_current_page(1)
 
         button1.connect('clicked', on_button1)
-        grid1.attach(button1, 0, 1, 1, 1)
+        grid1.attach(button1, 0, 2, 1, 1)
 
-        label2 = Gtk.Label(label='Synchronize with other device in account')
+        label2 = Gtk.Label(label='Synchronize device with account in other device')
         label2.set_alignment(0, 0.5)
         label2.set_justify(Gtk.Justification.LEFT)
-        grid1.attach(label2, 1, 2, 1, 1)
+        grid1.attach(label2, 1, 3, 1, 1)
 
         button2 = Gtk.Button.new_with_label('Synchronize account')
         def on_button2(res):
@@ -94,12 +100,12 @@ class SelectNewProfile(Gtk.Window):
             txaio.resolve(selected, self.SELECTED_SYNCRONIZE)
 
         button2.connect('clicked', on_button2)
-        grid1.attach(button2, 0, 2, 1, 1)
+        grid1.attach(button2, 0, 3, 1, 1)
 
         label3 = Gtk.Label(label='Recover account from account seed phrase')
         label3.set_alignment(0, 0.5)
         label3.set_justify(Gtk.Justification.LEFT)
-        grid1.attach(label3, 1, 3, 1, 1)
+        grid1.attach(label3, 1, 4, 1, 1)
 
         button3 = Gtk.Button.new_with_label('Recover account')
         def on_button3(res):
@@ -108,9 +114,63 @@ class SelectNewProfile(Gtk.Window):
             txaio.resolve(selected, self.SELECTED_RECOVER)
 
         button3.connect('clicked', on_button3)
-        grid1.attach(button3, 0, 3, 1, 1)
+        grid1.attach(button3, 0, 4, 1, 1)
 
-        notebook1.append_page(grid1)
+        self.append_page(grid1)
+
+        ##
+        ## page 2: select top-level action
+        ##
+        box2_1 = Gtk.VBox()
+
+        box2_2 = Gtk.HBox()
+        image2_1 = Gtk.Image()
+        image2_1.set_from_file('xbr_white.svg')
+        box2_2.add(image2_1)
+        box2_1.add(box2_2)
+
+        button2_1 = Gtk.Button.new_with_label('Generate seedphrase')
+        def on_button2_1(res):
+            sp = generate_seedphrase(strength=256, language='english')
+            textbuffer2_1.set_text(sp)
+        button2_1.connect('clicked', on_button2_1)
+        box2_1.add(button2_1)
+
+        label2_1 = Gtk.Label(label='Backup your new seed phrase in a secure offline location (e.g. on printed paper):')
+        label2_1.set_alignment(0, 0.5)
+        label2_1.set_justify(Gtk.Justification.LEFT)
+        box2_1.add(label2_1)
+
+        textview2_1 = Gtk.TextView()
+        textbuffer2_1 = textview2_1.get_buffer()
+        textbuffer2_1.set_text('\n' * 5)
+        textview2_1.set_editable(False)
+        textview2_1.set_justification(Gtk.Justification.CENTER)
+        textview2_1.set_monospace(True)
+        textview2_1.set_wrap_mode(Gtk.WrapMode.WORD)
+        box2_1.add(textview2_1)
+
+        self.append_page(box2_1)
+
+        ##
+        ## start on page 0
+        ##
+        # self.set_show_tabs(False)
+        self.set_current_page(0)
+
+    def on_apply_clicked(self, *args):
+        print("The 'Apply' button has been clicked")
+
+    def on_close_clicked(self, *args):
+        print("The 'Close' button has been clicked")
+        Gtk.main_quit()
+
+    def on_cancel_clicked(self, *args):
+        print("The Assistant has been cancelled.")
+        Gtk.main_quit()
+
+    def on_complete_toggled(self, checkbutton):
+        self.set_page_complete(self.complete, checkbutton.get_active())
 
 
 class MainWindow(Gtk.Window):
@@ -367,34 +427,35 @@ class Application(object):
 
             assert self._config and self._profile
 
+        d = txaio.create_future()
+
         if self._config:
             # if we have a config/profile, go on with ..
             win = MainWindow()
         else:
             # if we don't have a config yet,
-            # win = Assistant()
-            d = txaio.create_future()
             win = SelectNewProfile(config_path, 'default', d)
 
-            #selected = await d
-            #print(selected)
-
-            Gtk.main_quit()
-
         win.show_all()
-        win.connect("destroy", Gtk.main_quit)
 
+        return d
 
-
-
-from twisted.internet.task import react
 
 async def main(reactor, profile):
+    log = txaio.make_logger()
     app = Application()
-    app.start(profile)
+    d = app.start(profile)
+
+    def done(selected):
+        log.info('Selected action: {selected}', selected=selected)
+        Gtk.main_quit()
+
+    def error(err):
+        log.error('{err}', err=err)
+        sys.exit(1)
+
+    txaio.add_callbacks(d, done, error)
     Gtk.main()
 
-
-# reactor.run()
 
 react(main, ('default',))
