4570213670427ac984b6120b34cd369e9221fd02
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-03-27 13:00:42 +0100

    sync

--- examples/xbr/test_onboarding_ui.py
+++ examples/xbr/test_onboarding_ui.py
@@ -45,18 +45,47 @@ class SelectNewProfile(Gtk.Assistant):
     def __init__(self, config_path, profile_name, selected):
         Gtk.Assistant.__init__(self)
 
+        # configure assistant window/widget
         self.set_title("XBR Network")
         self.set_default_size(600, 600)
         self.set_border_width(50)
         self.set_resizable(False)
 
+        # assistant window/widget actions
         self.connect("cancel", self.on_cancel_clicked)
         self.connect("close", self.on_close_clicked)
         self.connect("apply", self.on_apply_clicked)
 
-        ##
-        ## page 1: select top-level action
-        ##
+        # setup assistant pages
+        self._setup_page1()
+        self._setup_page2()
+        self._setup_page3()
+
+        # start on page 1
+        self.set_current_page(0)
+
+    def on_apply_clicked(self, *args):
+        print("The 'Apply' button has been clicked")
+
+    def on_close_clicked(self, *args):
+        print("The 'Close' button has been clicked")
+        Gtk.main_quit()
+
+    def on_cancel_clicked(self, *args):
+        print("The Assistant has been cancelled.")
+        Gtk.main_quit()
+
+    def on_complete_toggled(self, checkbutton):
+        self.set_page_complete(self.complete, checkbutton.get_active())
+
+    def _setup_page1(self):
+        """
+        Setup page shown when no config/profile could be found. Allows to select from:
+
+        * new account
+        * synchronize device
+        * recover account
+        """
         grid1 = Gtk.Grid()
         grid1.set_row_spacing(20)
         grid1.set_column_spacing(20)
@@ -81,8 +110,6 @@ class SelectNewProfile(Gtk.Assistant):
         button1 = Gtk.Button.new_with_label('New account')
         def on_button1(res):
             self.log.info('SELECTED_NEW: {res}', res=res)
-            # self.destroy()
-            # txaio.resolve(selected, self.SELECTED_NEW)
             self.set_current_page(1)
 
         button1.connect('clicked', on_button1)
@@ -96,8 +123,7 @@ class SelectNewProfile(Gtk.Assistant):
         button2 = Gtk.Button.new_with_label('Synchronize account')
         def on_button2(res):
             self.log.info('SELECTED_SYNCRONIZE: {res}', res=res)
-            self.destroy()
-            txaio.resolve(selected, self.SELECTED_SYNCRONIZE)
+            self.set_current_page(2)
 
         button2.connect('clicked', on_button2)
         grid1.attach(button2, 0, 3, 1, 1)
@@ -110,17 +136,17 @@ class SelectNewProfile(Gtk.Assistant):
         button3 = Gtk.Button.new_with_label('Recover account')
         def on_button3(res):
             self.log.info('SELECTED_RECOVER: {res}', res=res)
-            self.destroy()
-            txaio.resolve(selected, self.SELECTED_RECOVER)
+            self.set_current_page(3)
 
         button3.connect('clicked', on_button3)
         grid1.attach(button3, 0, 4, 1, 1)
 
         self.append_page(grid1)
 
-        ##
-        ## page 2: select top-level action
-        ##
+    def _setup_page2(self):
+        """
+        Setup page shown to generate a new seed phrase.
+        """
         box2_1 = Gtk.VBox()
 
         box2_2 = Gtk.HBox()
@@ -133,6 +159,7 @@ class SelectNewProfile(Gtk.Assistant):
         def on_button2_1(res):
             sp = generate_seedphrase(strength=256, language='english')
             textbuffer2_1.set_text(sp)
+            checkbutton2_1.set_sensitive(True)
         button2_1.connect('clicked', on_button2_1)
         box2_1.add(button2_1)
 
@@ -150,27 +177,99 @@ class SelectNewProfile(Gtk.Assistant):
         textview2_1.set_wrap_mode(Gtk.WrapMode.WORD)
         box2_1.add(textview2_1)
 
+        box2_3 = Gtk.HBox()
+
+        checkbutton2_1 = Gtk.CheckButton(label="I have backed up my seed phrase")
+        checkbutton2_1.set_active(False)
+        checkbutton2_1.set_sensitive(False)
+        def on_checkbutton2_1(res):
+            button2_2.set_sensitive(True)
+        checkbutton2_1.connect("toggled", on_checkbutton2_1)
+        box2_3.add(checkbutton2_1)
+
+        button2_2 = Gtk.Button.new_with_label('Continue')
+        button2_2.set_sensitive(False)
+        def on_button2_2(res):
+            self.set_current_page(2)
+        button2_2.connect('clicked', on_button2_2)
+        box2_3.add(button2_2)
+
+        box2_1.add(box2_3)
+
         self.append_page(box2_1)
 
-        ##
-        ## start on page 0
-        ##
-        # self.set_show_tabs(False)
-        self.set_current_page(0)
+    def _setup_page3(self):
+        """
 
-    def on_apply_clicked(self, *args):
-        print("The 'Apply' button has been clicked")
+        :return:
+        """
+        box1 = Gtk.VBox()
 
-    def on_close_clicked(self, *args):
-        print("The 'Close' button has been clicked")
-        Gtk.main_quit()
+        box2 = Gtk.HBox()
+        image1 = Gtk.Image()
+        image1.set_from_file('xbr_white.svg')
+        box2.add(image1)
+        box1.add(box2)
 
-    def on_cancel_clicked(self, *args):
-        print("The Assistant has been cancelled.")
-        Gtk.main_quit()
+        grid1 = Gtk.Grid()
+        grid1.set_row_spacing(20)
+        grid1.set_column_spacing(20)
+        grid1.set_margin_top(20)
+        grid1.set_margin_bottom(20)
+        grid1.set_margin_start(20)
+        grid1.set_margin_end(20)
 
-    def on_complete_toggled(self, checkbutton):
-        self.set_page_complete(self.complete, checkbutton.get_active())
+        label1 = Gtk.Label(label='Your email address:')
+        grid1.attach(label1, 0, 0, 1, 1)
+
+        entry1 = Gtk.Entry()
+        entry1.set_text('')
+        entry1.set_max_length(255)
+        entry1.set_max_width_chars(40)
+        grid1.attach(entry1, 1, 0, 1, 1)
+
+        label2 = Gtk.Label(label='Your password:')
+        grid1.attach(label2, 0, 1, 1, 1)
+
+        entry2 = Gtk.Entry()
+        entry2.set_text('')
+        entry2.set_max_length(20)
+        entry2.set_max_width_chars(20)
+        entry2.set_visibility(False)
+        grid1.attach(entry2, 1, 1, 1, 1)
+
+        entry3 = Gtk.Entry()
+        entry3.set_text('')
+        entry3.set_max_length(20)
+        entry3.set_max_width_chars(20)
+        entry3.set_visibility(False)
+        grid1.attach(entry3, 1, 2, 1, 1)
+
+        label3 = Gtk.Label(label='EULA:')
+        grid1.attach(label3, 0, 3, 1, 1)
+
+        button1 = Gtk.CheckButton(label='I accept the EULA and terms of use')
+        button1.set_active(False)
+        button1.set_sensitive(True)
+
+        def on_button1(res):
+            print('1' * 100, res)
+            if button1.get_active():
+                button2.set_sensitive(True)
+        button1.connect('toggled', on_button1)
+        grid1.attach(button1, 1, 3, 1, 1)
+
+        button2 = Gtk.Button.new_with_label('Register')
+        button2.set_sensitive(False)
+
+        def on_button2(res):
+            print('2' * 100, res)
+        button2.connect('clicked', on_button2)
+        grid1.attach(button2, 2, 4, 1, 1)
+
+        box1.add(grid1)
+
+        self.append_page(box1)
 
 
 class MainWindow(Gtk.Window):
@@ -270,32 +369,6 @@ class MainWindow(Gtk.Window):
         self._win_grid.set_margin_end(20)
         self.add(self._win_grid)
 
-    def _test(self):
-        self._win_grid = Gtk.Grid()
-        self._win_grid.set_row_spacing(20)
-        self._win_grid.set_column_spacing(20)
-        self._win_grid.set_margin_top(20)
-        self._win_grid.set_margin_bottom(20)
-        self._win_grid.set_margin_start(20)
-        self._win_grid.set_margin_end(20)
-        self.add(self._win_grid)
-
-        self._win_user_email_label = Gtk.Label(label='Your email address:')
-        self._win_grid.attach(self._win_user_email_label, 0, 0, 1, 1)
-
-        self._win_user_email = Gtk.Entry()
-        self._win_user_email.set_text(profile.email)
-        self._win_user_email.set_max_length(255)
-        self._win_user_email.set_max_width_chars(40)
-        self._win_grid.attach(self._win_user_email, 1, 0, 1, 1)
-
-        self._win_eula_accept_label = Gtk.Label(label='XBR network EULA:')
-        self._win_grid.attach(self._win_eula_accept_label, 0, 1, 1, 1)
-
-        self._win_check_eula = Gtk.CheckButton(label='accept')
-        self._win_check_eula.connect('toggled', self.on_eula_toggled)
-        self._win_check_eula.set_active(False)
-        self._win_grid.attach(self._win_check_eula, 1, 1, 1, 1)
 
     def on_eula_toggled(self, button):
         value = button.get_active()
