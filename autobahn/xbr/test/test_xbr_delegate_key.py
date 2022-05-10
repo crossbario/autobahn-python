b2b588989e5dc3770fc2efef6a9a4cd180b9034a
Tobias Oberstein <tobias.oberstein@crossbario.com>
2022-05-10 18:02:57 +0200

    sync

--- autobahn/xbr/test/test_xbr_delegate_key.py
+++ autobahn/xbr/test/test_xbr_delegate_key.py
@@ -1,6 +1,33 @@
+###############################################################################
+#
+# The MIT License (MIT)
+#
+# Copyright (c) Crossbar.io Technologies GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+###############################################################################
+
 import os
 import sys
 from binascii import a2b_hex
+# from binascii import b2a_hex
 from unittest import skipIf
 
 from twisted.internet.defer import inlineCallbacks
@@ -14,7 +41,7 @@ from py_eth_sig_utils.signing import sign_typed_data, recover_typed_data
 from autobahn.xbr import HAS_XBR
 from autobahn.xbr import make_w3, EthereumKey
 from autobahn.xbr._eip712_member_register import _create_eip712_member_register
-
+from autobahn.xbr._eip712_market_create import _create_eip712_market_create
 
 # https://web3py.readthedocs.io/en/stable/providers.html#infura-mainnet
 HAS_INFURA = 'WEB3_INFURA_PROJECT_ID' in os.environ and len(os.environ['WEB3_INFURA_PROJECT_ID']) > 0
@@ -43,21 +70,6 @@ class TestEthereumKey(TestCase):
             '0xecdb40C2B34f3bA162C413CC53BA3ca99ff8A047',
             '0x2F070c2f49a59159A0346396f1139203355ACA43',
             '0x66290fA8ADcD901Fd994e4f64Cfb53F4c359a326',
-            '0xAA8Cc377db31a354137d8Bb86D0E38495dbD5266',
-            '0xeD22Fd82230B613a638e09E90dfBFeE6d604D3D2',
-            '0xfe79337902575cFF28f6c7B34ff718149fcC4849',
-            '0x8337Cd9Ec2e0aB3370DF3AA6D4cc29614ec9dDe4',
-            '0x29B6c56497CA179e9AAFD739BeBded3f23768903',
-            '0x163D58cE482560B7826b4612f40aa2A7d53310C4',
-            '0x72b3486d38E9f49215b487CeAaDF27D6acf22115',
-            '0x52d66f36A7927cF9612e1b40bD6549d08E0513Ff',
-            '0xFfa4F46DD8f537F3E73c4A8e056948856fa79Bf0',
-            '0x6231eECbA6e7983efe5ce6d16972E16cCcD97CE7',
-            '0x5f61F4c611501c1084738c0c8c5EbB5D3d8f2B6E',
-            '0xA6e693CC4A2b4F1400391a728D26369D9b82ef96',
-            '0xE51007F2bB31604Ddd531d0B48f8bFf5703755A8',
-            '0x60CC48BFC44b48A53e793FE4cB50e2d625BABB27',
-            '0xe78ea2fE1533D4beD9A10d91934e109A130D0ad8',
         ]
         self._keys = [
             '0x805f84af7e182359db0610ffb07c801012b699b5610646937704aa5cfc28b15e',
@@ -65,41 +77,41 @@ class TestEthereumKey(TestCase):
             '0x75848ddb1155cd1cdf6d74a6e7fbed06aeaa21ef2d8a05df7af2d95cdc127672',
             '0x5be599a34927a1110922d7704ba316144b31699d8e7f229e2684d5575a84214e',
             '0xc1bb7ce3481e95b28bb8c026667b6009c504c79a98e6c7237ba0788c37b473c9',
-            '0xbd7f02a1ca01492bfe63472adf185a5822a6bcd9686818b98e4da9dec87243cc',
-            '0x15d03ceae8d93dbdda6301385abc9e0ca07162671e20ce2133de712dbb30a5f0',
-            '0xbe32f0d4b6c5a8e03e68f5fa9dd755f572b77031ec8287f73366ad7ca18eafbe',
-            '0xd13669786f8e021a379ef4708b5ad538e5eb2305d36e05f93e08c95ae8af2a64',
-            '0xc14deab4c696362123a6792f5aa1b230fd552d5ae60eccb2e5b0c0bedc134fc1',
-            '0xc9b518d9cac2d2e3888bf430afcd3561d7b10bb14ffa85a87d445e206d7f7552',
-            '0x7fbbdc86c7c7d18e3778e4f0a067fa8f86963610fc9a103df53fc1d65bc052d1',
-            '0xc83702a545384c259da8542ae0184f9654dcfdb2025f6165c0b81c6e04b4c212',
-            '0x6c7ff5b6daccf5054d436733188f6147ab7b674bf7abc7c7feba0a9a70ec42e4',
-            '0x04139894d1f465fcaf295ec18044fb2eaaf9fcc7bf51190f816800fd7eab3479',
-            '0x521cbf9e86501aeb1bce0095eba6d234c16b9494aa2493fcb821c9d2b7c88cbc',
-            '0xe12ff0b82c9206b8c2c218e3f63fe94e6712c469da48a91aa192dacbb8cb75a5',
-            '0x60c2661dac98eb99b7dd6bd4f6a547ef105d0ed2879cf301f412267768cac56a',
-            '0xbdb81319735d3872e4a207c3b16a08fc7f963a34e0e178f5b85314c15dc24163',
-            '0x41e33a4387b4546a80839e2dafae469a08a59676244de08d012cf1f8d0f47e89',
         ]
 
-        # create EIP712 typed data dict from message data
-        verifying_contract = a2b_hex('0x2F070c2f49a59159A0346396f1139203355ACA43'[2:])
-        member = a2b_hex('0xA6e693CC4A2b4F1400391a728D26369D9b82ef96'[2:])
+        # create EIP712 typed data dicts from message data and schemata
+
+        verifying_contract = a2b_hex(self._addresses[0][2:])
+        member = a2b_hex(self._addresses[1][2:])
+        maker = a2b_hex(self._addresses[2][2:])
+        coin = a2b_hex(self._addresses[3][2:])
+
         eula = 'QmU7Gizbre17x6V2VR1Q2GJEjz6m8S1bXmBtVxS2vmvb81'
         profile = 'QmcNsPV7QZFHKb2DNn8GWsU5dtd8zH5DNRa31geC63ceb4'
+        terms = 'QmaozNR7DZHQK1ZcU9p7QdrshMvXqWK6gpu5rmrkPdT3L4'
+        meta = 'Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD'
+
+        market_id = a2b_hex('5b7ee23c9353479ca49a2461c0a1deb2')
+
         self._eip_data_objects = [
             _create_eip712_member_register(chainId=1, verifyingContract=verifying_contract, member=member,
                                            registered=666, eula=eula, profile=profile),
             _create_eip712_member_register(chainId=23, verifyingContract=a2b_hex(self._addresses[0][2:]),
-                                           member=a2b_hex(self._addresses[1][2:]), registered=9999, eula=eula, profile=''),
+                                           member=a2b_hex(self._addresses[1][2:]), registered=9999, eula=eula,
+                                           profile=profile),
+            _create_eip712_market_create(chainId=1, verifyingContract=verifying_contract, member=member, created=666,
+                                         marketId=market_id, coin=coin, terms=terms, meta=meta, maker=maker,
+                                         providerSecurity=10 ** 6, consumerSecurity=10 ** 6, marketFee=100),
         ]
         self._eip_data_obj_hashes = [
-            'e7e354cc11e83970374ab27c6a37282139ee7b531d7d5e291532c842e44035b4',
-            'a696e32dfebc67058a4db7490952e04313ffa05012575baab3ac946eb1749717',
+            '8abee87b2cf457841d173083d5f205183f3e78c6cee30ca77776344e11f612b3',
+            '6a4f10dc41080c445a86acaae652ce80878fe768f6b459af08d14465c5310138',
+            'f1b80df26ec6cc7dafeb8a5c69de77e8ec5a2c0e93f5d6e475124f18cf4c595f',
         ]
         self._eip_data_obj_signatures = [
-            '39cd6eec124908a9da2e42ff1e93baf97ad8a08865d60f5f00cdd0c233bcb73b14355c1e286d630be6f012c485f11b684adc6725fc010dd669f8131fb39c623e1b',
-            '7adf7ff9080cc650b5291bf3fb98271de0272599088778b7b00995706e7b5cab14f013ece0158596d7bd9022321955c8784dd8db3d7c38dee496c01c9bdc4bd91c',
+            '17ed35d8fd41fcb507ae11a3745d9775f37ff1c155257074fe2245cfb186f4336151fd018bf83a5e9902d825b645213a111630f78bbbc3c96f68d60b7e65dafd1c',
+            '1c0fa4d8e2b2d0d0391c4b7c5cf2f494eab5c7074aa46cfd11a2d8a6b8c087030db7a5b74128d9bb04f6baa12abaa45457e0cfe790e9ebbd62721c075d79335e1c',
+            '236660f4cc04df21289538bf15e83d5bd2858b9dad27022d6b83fc3374ce887d5789e1d40126823abf7ccef04d06e4a1717e6b6a00cbfacf5cc2e7b2e4cb384e1c',
         ]
 
     def test_key_from_seedphrase(self):
@@ -131,6 +143,7 @@ class TestEthereumKey(TestCase):
 
             # encode typed data dict and return message hash
             msg_hash = encode_typed_data(data)
+            # print('0' * 100, b2a_hex(msg_hash).decode())
             self.assertEqual(msg_hash, a2b_hex(self._eip_data_obj_hashes[i]))
 
             # sign message hash with private key
@@ -138,6 +151,7 @@ class TestEthereumKey(TestCase):
 
             # concatenate signature components into byte string
             signature = v_r_s_to_signature(*signature_vrs)
+            # print('1' * 100, b2a_hex(signature).decode())
 
             # ECDSA signatures in Ethereum consist of three parameters: v, r and s.
             # The signature is always 65-bytes in length.
@@ -157,6 +171,7 @@ class TestEthereumKey(TestCase):
 
             signature_vrs = sign_typed_data(data, key_raw)
             signature = v_r_s_to_signature(*signature_vrs)
+            # print('2' * 100, b2a_hex(signature).decode())
 
             self.assertEqual(len(signature), 65)
             self.assertEqual(signature, a2b_hex(self._eip_data_obj_signatures[i]))
