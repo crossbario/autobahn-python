48b26e3bf5af28d38dd395ff39b0de07906afb80
Tobias Oberstein <tobias.oberstein@gmail.com>
2023-06-01 11:32:58 +0200

    update min python

--- setup.py
+++ setup.py
@@ -300,7 +300,7 @@ setup(
 
     zip_safe=False,
 
-    python_requires='>=3.7',
+    python_requires='>=3.9',
 
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=["License :: OSI Approved :: MIT License",
@@ -311,8 +311,6 @@ setup(
                  "Operating System :: OS Independent",
                  "Programming Language :: Python",
                  "Programming Language :: Python :: 3",
-                 "Programming Language :: Python :: 3.7",
-                 "Programming Language :: Python :: 3.8",
                  "Programming Language :: Python :: 3.9",
                  "Programming Language :: Python :: 3.10",
                  "Programming Language :: Python :: 3.11",
